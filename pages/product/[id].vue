1cb80c89b609eb80c3a11f5628e235b16a42cbf3
asamiile <asamin47@gmail.com>
2025-09-07 15:37:06 +0900

    fix button color.

--- pages/product/[id].vue
+++ pages/product/[id].vue
@@ -77,7 +77,7 @@
         </div>
 
         <div v-if="user && product.creator_id === user.id" class="flex items-center gap-4 mt-8">
-          <NuxtLink :to="`/product/edit/${product.id}`" :class="buttonVariants({ variant: 'outline-gray', class: 'w-full' })">
+          <NuxtLink :to="`/product/edit/${product.id}`" :class="buttonVariants({ variant: 'outline', class: 'w-full' })">
             編集
           </NuxtLink>
           <UiButton @click="handleDelete" variant="destructive" class="w-full">
