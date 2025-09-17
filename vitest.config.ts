cde05e10f3e3443cfe8a35ffb78eebd2c5361ab5
asamiile <asamin47@gmail.com>
2025-09-17 23:39:21 +0900

    fix vitest config.

--- vitest.config.ts
+++ vitest.config.ts
@@ -1,24 +1,17 @@
 import { defineConfig } from 'vitest/config'
 import { defineVitestProject } from '@nuxt/test-utils/config'
-import { fileURLToPath } from 'node:url'
 
 export default defineConfig({
   test: {
     projects: [
-      defineVitestProject({
+      {
         test: {
           name: 'unit',
           include: ['test/{e2e,unit}/*.{test,spec}.ts'],
           environment: 'node',
         },
-        // Add alias resolution for the unit test project
-        resolve: {
-          alias: {
-            '~': fileURLToPath(new URL('./', import.meta.url)),
-          }
-        }
-      }),
-      defineVitestProject({
+      },
+      await defineVitestProject({
         test: {
           name: 'nuxt',
           include: ['test/nuxt/*.{test,spec}.ts'],
