e5d3b926d8e4793493dc1a4d174c2f17f91080bc
asamiile <asamin47@gmail.com>
2025-09-20 11:24:52 +0900

    remove storybook.

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -8,7 +8,6 @@ export default defineNuxtConfig({
     '@nuxtjs/supabase',
     'shadcn-nuxt',
     '@nuxt/test-utils/module',
-    '@nuxtjs/storybook',
   ],
   css: ['~/assets/css/tailwind.css'],
   vite: {
